423ceb6311a62527028410fb28f2bdff090601e4
Ilir Kosumi <i.kosumi@celonis.com>
2022-10-02 14:00:22 +0200

    Implement sumFn() that takes a func as 1st args and slice as 2nd and returns the result of the func operation on that slice

--- functions.go
+++ functions.go
@@ -5,10 +5,13 @@ import "fmt"
 func main() {
 	inputs := []int{1, 2, 3, 4}
 	fmt.Println("the inputs slice is", inputs)
-	fmt.Println("the sum of all values in inputs slice is", sum(inputs...))
+	fmt.Println("the sum of all values in inputs slice =>", sum(inputs...))
 
-	fmt.Println("all even numbers in inputs slice are", even(inputs...))
-	fmt.Println("the sum of even numbers in inputs slice is", sum(even(inputs...)...))
+	fmt.Println("all even numbers in inputs slice =>", even(inputs...))
+	fmt.Println("the sum of even numbers in inputs slice =>", sum(even(inputs...)...))
+
+	fmt.Println("the sum of all even values in input slice, using sumFn() =>", sumFn(even, inputs...))
+	fmt.Println("the sum of all odd values in input slice, using sumFn() =>", sumFn(odd, inputs...))
 }
 
 func sum(s ...int) int {
@@ -28,3 +31,28 @@ func even(n ...int) []int {
 	}
 	return result
 }
+
+func odd(n ...int) []int {
+	var result []int
+	for _, val := range n {
+		if val%2 != 0 {
+			result = append(result, val)
+		}
+	}
+	return result
+}
+
+func sumFn(fn func(n ...int) []int, xi ...int) int {
+	return sum(fn(xi...)...)
+}
+
+/*
+func sum2(<fn>, <slice>) {...}
+
+e.g.
+func sum2(even, []int{1, 2, 3, 4})
+=> 6
+
+func sum2(odd, []int{1, 2, 3, 4})
+=> 4
+*/
